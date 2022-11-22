d943c36c1c929c0c89853a56f2361357c39f2632
chernyds22pv192 <113597343+chernyds22pv192@users.noreply.github.com>
2022-11-22 21:37:38 +0300

    MangdLib

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Home } from './pages/home';
+import { PageManga } from './pages/mangaPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="wrapper">
+
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={ <Home /> } />
+        <Route path={'/pageManga'} element={ <PageManga/> } />
+      </Routes>
     </div>
+    <Footer/></>
   );
 }
 
